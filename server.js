5b3477e96fe56299f339905f9befdd0a82967eee
elazar matan <elazarmata8@gmail.com>
2025-08-11 17:31:55 +0300

    ``` feat: Add request logging middleware
    This commit introduces a middleware function that logs the HTTP method and URL of each incoming request to the console. This provides valuable insight into server activity and aids in debugging.
    ```

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import router from './routes/complaints.js'
 import {checkPassword} from './routes/complaints.js'
 
 const server = express()
+server.use((req,res,next) => {
+    console.log(`method ${req.method} url ${req.url}`)
+    next()
+})
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'))
 server.use('/idf/admin',checkPassword)
 server.use(express.json())
 server.use('/idf',router)
-
 
 server.listen(process.env.PORT,() => console.log(`server listening on port ${process.env.PORT}`))
